fc0842747f403ec620cf350dcb60e1fc3c912a8d
Kerry <aca.kerry.li@gmail.com>
2022-10-28 16:54:57 -0400

    updates dependency in requirements

--- api/api.py
+++ api/api.py
@@ -1,7 +1,16 @@
 from flask import Flask
+import db
 
 app = Flask(__name__)
 
 @app.route('/greeting')
 def greet():
     return "hello"
+
+@app.route("/test")
+def test():
+    db.db.collection.insert_one({"name": "John"})
+    return "Connected to the data base!"
+
+if __name__ == "main":
+    app.run()
