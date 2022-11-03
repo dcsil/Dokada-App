ffddb5d8a99b41a6723ab8dccc59f3cbf4cc24cd
kamrandb <kamranbadirov@gmail.com>
2022-11-03 19:12:17 -0400

    navbar ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './styles/App.css';
 import { Outlet } from 'react-router-dom';
-import Navbar from "./Navbar.jsx"
+import Menu from "./Menubar.jsx"
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Menu/>
       <Outlet/>
     </div>
   );
