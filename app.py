42caa2b53dda043a42198d55c0bb53a96fbe471c
Martin Chak <martin.chak@mail.utoronto.ca>
2022-11-07 00:23:09 -0500

    Fixed flask react router linking

--- app.py
+++ app.py
@@ -3,20 +3,28 @@ from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
 #from api.greet import greet
 
-app = Flask(__name__, static_url_path='', static_folder='build')
+app = Flask(__name__, static_url_path='/', static_folder='build')
 CORS(app) #comment this on deployment
 api = Api(app)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
+@app.errorhandler(404)
+def not_found(e):
     return send_from_directory(app.static_folder,'index.html')
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder,'index.html')
+
 
 """
 @app.route("/", defaults={'path':''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
-"""
     
+
+@app.route("/canvas", defaults={'path':''})
+def serve_canvas(path):
+    return send_from_directory(app.static_folder,'index.html')
+"""
 
 #api.add_resource(greet, '/greeting')
