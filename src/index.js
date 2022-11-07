7c42c09b217c54bcc2bed7ec86ddabdb11d1ac9a
kamrandb <kamranbadirov@gmail.com>
2022-11-06 20:25:55 -0500

    making better

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CanvasPage from './CanvasPage';
 import HomePage from './HomePage';
 import Dasboard from './Dashboard';
 import Auth from "./components/Auth.jsx"
@@ -16,7 +15,6 @@ root.render(
       <Routes>
         <Route path="/" element={<App/>}>
           <Route index element={<HomePage/>} />
-          <Route path="canvas" element={<CanvasPage/>} />
           <Route path="dashboard" element={<Dasboard/>} />
           <Route path="login" element={<Auth/>} />
         </Route>
