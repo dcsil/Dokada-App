b7cadd85d13a0ba637404bf2db849c12a298a5b0
kerry2021 <kerry.li@mail.utoronto.ca>
2022-11-16 21:03:04 -0500

    testdb setup

--- api/db.py
+++ api/db.py
@@ -3,7 +3,7 @@ from flask_pymongo import pymongo
 
 CONNECTION_STRING = "mongodb+srv://dokada:123@cluster0.aiofrm2.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('Deployment')
+db = client.get_database('TestDB')
 user_collection = pymongo.collection.Collection(db, 'imageData')
 
 def getCollection():
