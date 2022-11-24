6fa29e525cef00085fec433fb0c4aeafdc680202
kerry2021 <kerry.li@mail.utoronto.ca>
2022-11-23 19:14:51 -0500

    remove exposed connection credentials

--- api/db.py
+++ api/db.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask_pymongo import pymongo
+from dotenv import load_dotenv
+import os
 
-CONNECTION_STRING = "mongodb+srv://dokada:123@cluster0.aiofrm2.mongodb.net/?retryWrites=true&w=majority"
+load_dotenv()
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+
+CONNECTION_STRING = "mongodb+srv://" + USER + ":" + PASSWORD + "@cluster0.aiofrm2.mongodb.net/?retryWrites=true&w=majority"
+print(CONNECTION_STRING)
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('TestDB')
 #user_collection = pymongo.collection.Collection(db, 'imageData')
