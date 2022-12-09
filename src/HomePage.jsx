e7ee9344bb4e1271eb58abad995b56af6cc742c5
Martin Chak <martin.chak@mail.utoronto.ca>
2022-12-09 01:52:47 -0500

    Remove artifact

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -13,7 +13,7 @@ function HomePage(props) {
         Authorization: "Bearer " + props.token,
       },
     })
-      .then((response) => {c8a934b6
+      .then((response) => {
         const res = response.json();
         res.access_token && props.setToken(res.access_token);
       })
